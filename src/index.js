07cbc61655421fbb8bf74dc626638b04701d7e49
acurtler <curt8897@stthomas.edu>
2024-03-29 11:38:29 -0500

    css

--- src/index.js
+++ src/index.js
@@ -86,7 +86,7 @@ app.listen(PORT, () => {
 module.exports = pool;
 */
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'css')));
 
 // Middleware to set the correct MIME type for CSS files
 app.use((req, res, next) => {
