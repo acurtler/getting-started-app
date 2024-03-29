c1ea4e2df4e7649a2adcf30e1bc50ce57b6c730b
acurtler <curt8897@stthomas.edu>
2024-03-29 10:51:29 -0500

    db options

--- src/index.js
+++ src/index.js
@@ -39,7 +39,7 @@ const dbOptions = {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT // If applicable
+    //port: process.env.DB_PORT // If applicable
 };
 
 // Create a database connection pool with retry logic
